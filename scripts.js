3094f48031f2a93c42dd3b4dbfd6ad703a48ddb4
craftont <146998883+craftont@users.noreply.github.com>
2024-04-21 18:47:28 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.flex-container');
+
+    container.style.padding = '10px';
+    container.style.border = '5px solid #795548';  
+    container.style.borderRadius = '15px';  
+});
